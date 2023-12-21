3fc7060f578786057ec117aee69f884ed6ec6db5
Jenny Ouk <jenny.ouk@gmail.com>
2023-12-21 12:18:39 -0800

    add gradient header

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import { Login } from './Login.jsx';
 import { Register } from './Register.jsx';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 
 export const Entry = (props) => {
   const [currentForm, setCurrentForm] = useState('login');
@@ -8,15 +21,36 @@ export const Entry = (props) => {
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   };
+  const theme = useTheme();
 
   return (
-    <div className='Entry'>
-      <h1>c l a y d a y</h1>
-      {currentForm === 'login' ? (
-        <Login onFormSwitch={toggleForm} login={props.login}/>
-      ) : (
-        <Register onFormSwitch={toggleForm} login={props.login} />
-      )}
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Typography
+          variant='h1'
+          align='center'
+          color='grey.700'
+          sx={{
+            backgroundcolor: 'primary',
+            backgroundImage: `linear-gradient(45deg, #5514B4, #FF80FF)`,
+            backgroundSize: '100%',
+            // backgroundRepeat: 'repeat',
+            // backgroundClip: 'text',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            mt: 3,
+            mb: 5,
+          }}
+        >
+          clayday
+        </Typography>
+        {currentForm === 'login' ? (
+          <Login onFormSwitch={toggleForm} login={props.login} />
+        ) : (
+          <Register onFormSwitch={toggleForm} login={props.login} />
+        )}
+      </Container>
+    </ThemeProvider>
   );
 };
